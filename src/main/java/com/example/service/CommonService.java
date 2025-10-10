5de3aa5f3f8fb24b2185884edf407e675671a02c
Vlad <0x01code@gmail.com>
2025-10-10 21:20:31 +0300

    Update response message in CommonService to reflect version 2.1

--- src/main/java/com/example/service/CommonService.java
+++ src/main/java/com/example/service/CommonService.java
@@ -9,7 +9,7 @@ public class CommonService {
 
     public String processRequest(Map<String, Object> input) {
         // Общая бизнес-логика для всех облаков
-        return "Hello from Lambda v2.0";
+        return "Hello from Lambda v2.1";
     }
 
     public String processRequest() {
