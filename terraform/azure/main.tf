5c8082a674b85e59969d57d351fdd8eda8dd5fbb
Vlad <0x01code@gmail.com>
2025-10-15 16:36:37 +0300

    Refactor resource names to use variable inputs for improved configurability

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -21,8 +21,8 @@ resource "azurerm_app_service_plan" "lambda_plan" {
   resource_group_name = azurerm_resource_group.lambda_rg.name
   kind                = "FunctionApp"
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
 
